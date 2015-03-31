56ee9052fb6cb9ade2f1acdfb8ff068c455b7412
Ross Hettel <rhettel@highground.com>
2015-03-31 16:13:46 -0500

    added wrapper ctrl

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,8 @@
 angular.module('ngWeatherStation').config(['$routeProvider', function ($routeProvider) {
 	$routeProvider
+		.when('/', {
+			templateUrl: 'templates/init.html'
+		})
 		.when('/leftWeather', {
 			templateUrl: 'templates/leftWeather.html',
 			controller: 'weatherCtrl'
---
+++ app/wrapper.ctrl.js
@@ -0,0 +1,5 @@
+angular.module('ngWeatherStation').controller('wrapperCtrl', 
+	['$route', function ($route) {
+		console.log($route.routes);
+	}];
+);
