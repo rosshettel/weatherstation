865959205cb638b8616a37e9a382b1450209a18a
Ross Hettel <rhettel@highground.com>
2015-05-06 09:27:28 -0500

    added loop cam

--- static/app/routes.js
+++ static/app/routes.js
@@ -2,6 +2,7 @@ angular.module('ngWeatherStation').config(['$routeProvider', function ($routePro
     var weatherRadar = 'http://api.wunderground.com/api/25d29ac708c38d34/animatedradar/q/60614.gif?width=640&height=480&newmaps=1&smooth=1&noclutter=1&timelabel=1',
 	   noaaWebcam = 'http://www.glerl.noaa.gov/metdata/chi/chi1.jpg',
 	   grantParkCam = 'http://cdn.abclocal.go.com/three/wls/webcam/ColumbiaCam.jpg',
+	   loopCam = 'http://cdn.abclocal.go.com/three/wls/webcam/Loopscape.jpg',
         imageChannel = function (imgSrc) {
         return {
             templateUrl: 'static/templates/fullScreenImage.html',
@@ -27,5 +28,6 @@ angular.module('ngWeatherStation').config(['$routeProvider', function ($routePro
           })
          .when('/weatherRadar', imageChannel(weatherRadar))
          .when('/noaaWebcam', imageChannel(noaaWebcam))
-	    .when('/grantParkCam', imageChannel(grantParkCam));
+	    .when('/grantParkCam', imageChannel(grantParkCam))
+	    .when('/loopCam', imageChannel(loopCam));
 }]);
