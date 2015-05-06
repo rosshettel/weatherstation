82bc6423084fc5cc95eaff35fe7db0530f08ef03
Ross Hettel <rhettel@highground.com>
2015-05-05 22:57:51 -0500

    still showing scroll bars

--- static/app/routes.js
+++ static/app/routes.js
@@ -15,7 +15,7 @@ angular.module('ngWeatherStation').config(['$routeProvider', function ($routePro
 		   templateUrl: 'static/templates/fullScreenImage.html',
              controller: 'fullScreenImageCtrl',
              resolve: {
-                 imageUrl: function () { return 'http://api.wunderground.com/api/25d29ac708c38d34/animatedradar/q/60614.gif?width=640&height=480&newmaps=1&smooth=1&noclutter=1&timelabel=1'; }
+                 imageUrl: function () { return 'http://api.wunderground.com/api/25d29ac708c38d34/animatedradar/q/60614.gif?width=620&height=460&newmaps=1&smooth=1&noclutter=1&timelabel=1'; }
              }
 	    });
 }]);
