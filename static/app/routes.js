61a52ae2abb2d43fd66c4b9f49fc4175e5b56541
Ross Hettel <rhettel@highground.com>
2015-05-06 00:06:35 -0500

    added more webcams

--- static/app/routes.js
+++ static/app/routes.js
@@ -1,21 +1,31 @@
 angular.module('ngWeatherStation').config(['$routeProvider', function ($routeProvider) {
-	$routeProvider
-		.when('/', {
-			templateUrl: 'static/templates/init.html'
-		})
-		.when('/leftWeather', {
-			templateUrl: 'static/templates/leftWeather.html',
-			controller: 'weatherCtrl'
-		})
-		.when('/rightWeather', {
-			templateUrl: 'static/templates/rightWeather.html',
-			controller: 'weatherCtrl'
-		})
-	    .when('/weatherRadar', {
-		   templateUrl: 'static/templates/fullScreenImage.html',
-             controller: 'fullScreenImageCtrl',
-             resolve: {
-                 imageUrl: function () { return 'http://api.wunderground.com/api/25d29ac708c38d34/animatedradar/q/60614.gif?width=640&height=480&newmaps=1&smooth=1&noclutter=1&timelabel=1'; }
-             }
-	    });
+    var weatherRadar = 'http://api.wunderground.com/api/25d29ac708c38d34/animatedradar/q/60614.gif?width=640&height=480&newmaps=1&smooth=1&noclutter=1&timelabel=1',
+	   noaaWebcam = 'http://www.glerl.noaa.gov/metdata/chi/chi1.jpg',
+	   grantParkCam = 'http://cdn.abclocal.go.com/three/wls/webcam/ColumbiaCam.jpg',
+        imageChannel = function (imgSrc) {
+        return {
+            templateUrl: 'static/templates/fullScreenImage.html',
+            controller: 'fullScreenImageCtrl',
+            resolve: {
+                imageUrl: function () { return imgSrc; }
+            }
+        };
+    };
+
+
+     $routeProvider
+          .when('/', {
+               templateUrl: 'static/templates/init.html'
+          })
+          .when('/leftWeather', {
+               templateUrl: 'static/templates/leftWeather.html',
+               controller: 'weatherCtrl'
+          })
+          .when('/rightWeather', {
+               templateUrl: 'static/templates/rightWeather.html',
+               controller: 'weatherCtrl'
+          })
+         .when('/weatherRadar', imageChannel(weatherRadar))
+         .when('/noaaWebcam', imageChannel(noaaWebcam))
+	    .when('/grantParkCam', imageChannel(grantParkCam));
 }]);
