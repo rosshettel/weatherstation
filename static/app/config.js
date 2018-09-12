f6de0566119ebfc26259c8a0e4dca142f2b62ae3
Ross Hettel <ross@het.tel>
2018-09-12 16:17:40 -0700

    reenable for dashboard

--- static/app/config.js
+++ static/app/config.js
@@ -1,15 +1,15 @@
 angular.module('weatherstation').constant('config', {
     webcams: [
         'http://wx.koin.com/weather/images/Eastside_Exchange.jpg',
-        // 'http://wx.koin.com/weather/images/Riverview_Bank.jpg',
-        // 'http://cdn.tegna-media.com/kgw/weather/wellsfargo.jpg',
+        'http://wx.koin.com/weather/images/Riverview_Bank.jpg',
+        'http://cdn.tegna-media.com/kgw/weather/wellsfargo.jpg',
         'http://cdn.tegna-media.com/kgw/weather/rosecity.jpg',
         'http://w3.gorge.net/niknas/webcam.jpg',
         'http://wx.koin.com/weather/images/Skamania_Lodge.jpg',
-        // 'https://www.fsvisimages.com/images/photos-main/CORI1_main.jpg',
-        // 'https://tripcheck.com/RoadCams/cams/i84metro_pid588.jpg',
-        // 'https://tripcheck.com/RoadCams/cams/fremontbridge_pid531.jpg',
-        // 'https://tripcheck.com/RoadCams/cams/US30%20at%20St%20Johns%20Bridge%20Top_pid3487.JPG'
+        'https://www.fsvisimages.com/images/photos-main/CORI1_main.jpg',
+        'https://tripcheck.com/RoadCams/cams/i84metro_pid588.jpg',
+        'https://tripcheck.com/RoadCams/cams/fremontbridge_pid531.jpg',
+        'https://tripcheck.com/RoadCams/cams/US30%20at%20St%20Johns%20Bridge%20Top_pid3487.JPG'
     ],
     wundergroundTokens: ['d0dba01007c9d499'],
     darkSkyKey: 'fb7530eb012c152629f2ace23921214e',
